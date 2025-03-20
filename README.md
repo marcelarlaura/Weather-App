8dc5d02155dafc4ed964129e73b09ec300db42dd
marcelarlaura <171378249+marcelarlaura@users.noreply.github.com>
2025-03-20 14:49:00 -0500

    display temperature per hour for current day

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 The weather app allows to search upon any city around the world and, 
 through apis, the weather-related data will be relfected on the user interface.
+
+Also, I'm giving credits for picture of sky <a href="https://unsplash.com/es/@mhmcmurrin?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">by Maddison McMurrin</a> in <a href="https://unsplash.com/es/fotos/nubes-blancas-durante-el-dia-GDumtPpJsT4?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+      
--- javascript.js
+++ javascript.js
@@ -7,6 +7,9 @@ let temperature = document.querySelector('.current-temp');
 let minTemp = document.querySelector('.minTemp');
 let maxTemp = document.querySelector('.maxTemp');
 let img = document.querySelector('img');
+let condition = document.querySelector('.condition');
+let section3 = document.querySelector('.section3');
+
 
 
 input.addEventListener('input', ()=>{
@@ -29,11 +32,14 @@ async function givenLocation(city){
         let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/today?unitGroup=metric&key=MUBVBGDSWHC82R9W4ERRGKRH2&contentType=json`;
         const response = await fetch(url);
         const response2 = await response.json();
+        let now = new Date();
+        let minutes = now.getMinutes();
         loc.textContent = '⟟'+response2.resolvedAddress;
-        date.textContent =  response2.days[0].datetime +' , '+ response2.currentConditions.datetime[0]+response2.currentConditions.datetime[1] + response2.currentConditions.datetime[2]+response2.currentConditions.datetime[3] + response2.currentConditions.datetime[4] + ' hrs ';
+        date.textContent =  response2.days[0].datetime +' , '+ response2.currentConditions.datetime[0]+response2.currentConditions.datetime[1] + response2.currentConditions.datetime[2]+ minutes + ' hrs ';
         temperature.textContent = response2.currentConditions.temp +' °C';
         minTemp.textContent = 'Range: '+ response2.days[0].tempmin +' °C / ';
         maxTemp.textContent = '  '+ response2.days[0].tempmax +' °C';
+        condition.textContent = response2.currentConditions.conditions;
         console.log(response2);
     } catch {
         loc.textContent = 'Non-available location';
@@ -53,7 +59,7 @@ async function givenLocation2(city){
             let gif2 = await gif.json();
             img.src =  gif2.data.images.original.url;
         } catch {
-            console.log('Image not found');
+            img.src = 'sky.jpg';
         }
 
     }catch{
@@ -61,10 +67,36 @@ async function givenLocation2(city){
     }
 }
 
+
+async function givenLocation3(city){
+
+    try{
+        let response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/today?unitGroup=metric&key=MUBVBGDSWHC82R9W4ERRGKRH2&contentType=json`);
+        let response2 = await response.json();
+        for (let i=0; i<12; i++){
+            let div = document.createElement('div');
+            div.textContent = response2.days[0].hours[i+1].temp + ' °C';
+            section3.append(div);
+        }
+        
+    } catch {
+        for (let i=0; i<12; i++){
+            let div = document.createElement('div');
+            div.textContent = 'N/A';
+            section3.append(div);
+        }
+    }
+}
+
+
+
+
 button.addEventListener('click', ()=>{
     if (input.value.match(/^[A-Za-z]+$/)){
         let city = input.value.toLowerCase();
         givenLocation(city);
         givenLocation2(city);
+        givenLocation3(city);
+        
     }
 })
